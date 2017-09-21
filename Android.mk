9743ec86bacc90bb1b6a645c92d202feff993c09
Janboe Ye <yeyuanbo@xiaomi.com>
2017-09-21 21:30:55 +0800

    fix build erro
    Signed-off-by: Janboe Ye <yeyuanbo@xiaomi.com>

--- Android.mk
+++ Android.mk
@@ -28,122 +28,17 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES :=  \
 	$(OPENCL_HEADER_PATH) \
+	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)/src
 
 LOCAL_CFLAGS := \
-	-Wno-unused-parameter
+	-Wno-unused-parameter -std=c++11 -O2 -Wall -Wno-comment -Wno-return-type -Wno-switch -Wno-missing-noreturn
+
+LOCAL_NDK_STL_VARIANT := gnustl_static
+
+LOCAL_RTTI_FLAG := -frtti -fexceptions
 
 LOCAL_SRC_FILES :=  \
-     src/routines/common.cpp  \
-     src/routines/level1/xaxpy.cpp  \
-     src/routines/level1/xswap.cpp  \
-     src/routines/level1/xamax.cpp  \
-     src/routines/level1/xnrm2.cpp  \
-     src/routines/level1/xdotu.cpp  \
-     src/routines/level1/xcopy.cpp  \
-     src/routines/level1/xscal.cpp  \
-     src/routines/level1/xdot.cpp  \
-     src/routines/level1/xdotc.cpp  \
-     src/routines/level1/xasum.cpp  \
-     src/routines/levelx/xim2col.cpp  \
-     src/routines/levelx/xgemmbatched.cpp  \
-     src/routines/levelx/xinvert.cpp  \
-     src/routines/levelx/xaxpybatched.cpp  \
-     src/routines/levelx/xomatcopy.cpp  \
-     src/routines/level3/xsymm.cpp  \
-     src/routines/level3/xgemm.cpp  \
-     src/routines/level3/xhemm.cpp  \
-     src/routines/level3/xtrmm.cpp  \
-     src/routines/level3/xher2k.cpp  \
-     src/routines/level3/xherk.cpp  \
-     src/routines/level3/xtrsm.cpp  \
-     src/routines/level3/xsyr2k.cpp  \
-     src/routines/level3/xsyrk.cpp  \
-     src/routines/level2/xhpmv.cpp  \
-     src/routines/level2/xhpr2.cpp  \
-     src/routines/level2/xhemv.cpp  \
-     src/routines/level2/xhpr.cpp  \
-     src/routines/level2/xsyr.cpp  \
-     src/routines/level2/xhbmv.cpp  \
-     src/routines/level2/xsbmv.cpp  \
-     src/routines/level2/xspmv.cpp  \
-     src/routines/level2/xher2.cpp  \
-     src/routines/level2/xsyr2.cpp  \
-     src/routines/level2/xtpmv.cpp  \
-     src/routines/level2/xspr2.cpp  \
-     src/routines/level2/xgerc.cpp  \
-     src/routines/level2/xsymv.cpp  \
-     src/routines/level2/xspr.cpp  \
-     src/routines/level2/xtrmv.cpp  \
-     src/routines/level2/xgeru.cpp  \
-     src/routines/level2/xger.cpp  \
-     src/routines/level2/xgbmv.cpp  \
-     src/routines/level2/xtrsv.cpp  \
-     src/routines/level2/xtbmv.cpp  \
-     src/routines/level2/xgemv.cpp  \
-     src/routines/level2/xher.cpp  \
-     src/clblast.cpp  \
-     src/routine.cpp  \
-     src/utilities/utilities.cpp  \
-     src/utilities/clblast_exceptions.cpp  \
-     src/cache.cpp  \
-     src/clblast_c.cpp  \
-     src/database/database.cpp  \
-     src/routines/common.cpp  \
-     src/routines/level1/xaxpy.cpp  \
-     src/routines/level1/xswap.cpp  \
-     src/routines/level1/xamax.cpp  \
-     src/routines/level1/xnrm2.cpp  \
-     src/routines/level1/xdotu.cpp  \
-     src/routines/level1/xcopy.cpp  \
-     src/routines/level1/xscal.cpp  \
-     src/routines/level1/xdot.cpp  \
-     src/routines/level1/xdotc.cpp  \
-     src/routines/level1/xasum.cpp  \
-     src/routines/levelx/xim2col.cpp  \
-     src/routines/levelx/xgemmbatched.cpp  \
-     src/routines/levelx/xinvert.cpp  \
-     src/routines/levelx/xaxpybatched.cpp  \
-     src/routines/levelx/xomatcopy.cpp  \
-     src/routines/level3/xsymm.cpp  \
-     src/routines/level3/xgemm.cpp  \
-     src/routines/level3/xhemm.cpp  \
-     src/routines/level3/xtrmm.cpp  \
-     src/routines/level3/xher2k.cpp  \
-     src/routines/level3/xherk.cpp  \
-     src/routines/level3/xtrsm.cpp  \
-     src/routines/level3/xsyr2k.cpp  \
-     src/routines/level3/xsyrk.cpp  \
-     src/routines/level2/xhpmv.cpp  \
-     src/routines/level2/xhpr2.cpp  \
-     src/routines/level2/xhemv.cpp  \
-     src/routines/level2/xhpr.cpp  \
-     src/routines/level2/xsyr.cpp  \
-     src/routines/level2/xhbmv.cpp  \
-     src/routines/level2/xsbmv.cpp  \
-     src/routines/level2/xspmv.cpp  \
-     src/routines/level2/xher2.cpp  \
-     src/routines/level2/xsyr2.cpp  \
-     src/routines/level2/xtpmv.cpp  \
-     src/routines/level2/xspr2.cpp  \
-     src/routines/level2/xgerc.cpp  \
-     src/routines/level2/xsymv.cpp  \
-     src/routines/level2/xspr.cpp  \
-     src/routines/level2/xtrmv.cpp  \
-     src/routines/level2/xgeru.cpp  \
-     src/routines/level2/xger.cpp  \
-     src/routines/level2/xgbmv.cpp  \
-     src/routines/level2/xtrsv.cpp  \
-     src/routines/level2/xtbmv.cpp  \
-     src/routines/level2/xgemv.cpp  \
-     src/routines/level2/xher.cpp  \
-     src/clblast.cpp  \
-     src/routine.cpp  \
-     src/utilities/utilities.cpp  \
-     src/utilities/clblast_exceptions.cpp  \
-     src/cache.cpp  \
-     src/clblast_c.cpp  \
-     src/database/database.cpp  \
      src/routines/common.cpp  \
      src/routines/level1/xaxpy.cpp  \
      src/routines/level1/xswap.cpp  \
@@ -209,20 +104,11 @@ LOCAL_MODULE := libclbast
 
 LOCAL_MODULE_TAGS := eng optional tests
 
-LOCAL_ARM_MODE := arm
-
-LOCAL_MULTILIB := both
-
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin/
-
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT)/bin/clinfo64
-
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/bin/clinfo32
-
 # Mark source files as dependent on Android.mk
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
-include $(BUILD_EXECUTABLE)
+
+include $(BUILD_SHARED_LIBRARY)
 
 
 #-----------------
